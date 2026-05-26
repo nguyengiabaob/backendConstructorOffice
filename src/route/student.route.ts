3a1d442bc54de043dd0d65677383223c4dbaa45f
baong <baong@vntt.com.vn>
2026-05-26 12:57:08 +0700

    deploy 6

--- src/route/student.route.ts
+++ src/route/student.route.ts
@@ -3,16 +3,6 @@ import studentController from "../controllers/student.controller";
 
 const route = Router();
 
-route.post("/addStudent", studentController.addStudent);
-
-route.post("/students", studentController.assignLesson);
-
-route.put("/update", studentController.updateStudent);
-route.delete("/delete:phone", studentController.deleteStudent);
-
 route.get("/getAll", studentController.getStudents);
-route.get("/get/:phone", studentController.getStudentsById);
-
-route.get("/editProfile", studentController.editStudent);
 
 export default route;
